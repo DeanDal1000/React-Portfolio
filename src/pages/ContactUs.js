491529f9463756fb47135a94e469aa3e60f54e63
DeanDal1000 <dalzelldean@gmail.com>
2021-04-27 14:52:34 +0100

    Added Contact Form

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -5,6 +5,17 @@ import { pageAnimation, titleAnimation } from '../components/animation';
 import {
   Circle,
   ContactStyle,
+  Container,
+  Form,
+  FormBody,
+  FormContact,
+  FormInput,
+  FormItem,
+  FormItemLeft,
+  FormLeft,
+  FormName,
+  FormTitle,
+  FormTitleButton,
   Hide,
   Social,
   Title,
@@ -21,15 +32,46 @@ const ContactUs = () => {
     >
       <Title>
         <Hide>
-          <motion.h2 variants={titleAnimation}>Hello</motion.h2>
+          <motion.h2 variants={titleAnimation}>Contact Me</motion.h2>
         </Hide>
       </Title>
-      <div>
-        <Social>
-          <Circle />
-          <h2>Hello</h2>
-        </Social>
-      </div>
+      <Container>
+        <Form>
+          <FormTitle>
+            <FormLeft>
+              <FormTitleButton />
+            </FormLeft>
+          </FormTitle>
+          <FormBody>
+            <FormItemLeft>
+              <FormName>
+                <span>CONTACT</span>
+                <span>ME</span>
+              </FormName>
+              <FormContact>Hello</FormContact>
+            </FormItemLeft>
+            <FormItem>
+              <FormInput>
+                <div className="group">
+                  <input class="input" placeholder="NAME" />
+                </div>
+                <div className="group">
+                  <input class="input" placeholder="EMAIL" />
+                </div>
+                <div className="group">
+                  <input class="input" placeholder="CONTACT NO" />
+                </div>
+                <div className="group-message">
+                  <input class="input" placeholder="MESSAGE" />
+                </div>
+                <div className="form-button">
+                  <button id="send">SEND</button>
+                </div>
+              </FormInput>
+            </FormItem>
+          </FormBody>
+        </Form>
+      </Container>
       <ScrollTop />
     </ContactStyle>
   );
--- src/styles/Contact.element.js
+++ src/styles/Contact.element.js
@@ -37,3 +37,138 @@ export const Social = styled(motion.div)`
     margin: 2rem;
   }
 `;
+
+export const Container = styled.div`
+  display: flex;
+  margin: auto;
+  padding: 10px;
+  justify-content: center;
+  @media (max-width: 700px) {
+    flex-direction: column;
+  }
+`;
+
+export const Form = styled.div`
+  position: relative;
+  background: #3e3e3e;
+  border-radius: 15px;
+  width: 50%;
+
+  &:after {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 20px;
+    right: 20px;
+    bottom: 0;
+    border-radius: 15px;
+    box-shadow: 0 20px 40px rgba(0, 0, 0, 0.4);
+    z-index: -1;
+  }
+  @media (max-width: 700px) {
+    flex-direction: column;
+    width: 100%;
+  }
+`;
+
+export const FormTitle = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px 20px;
+  background: #4d4d4f;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+`;
+
+export const FormLeft = styled.div`
+  margin-right: auto;
+`;
+
+export const FormTitleButton = styled.div`
+  display: inline-block;
+  width: 8px;
+  height: 8px;
+  margin-right: 3px;
+  border-radius: 8px;
+  background: white;
+`;
+
+export const FormBody = styled.div`
+  display: flex;
+`;
+
+export const FormItem = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+export const FormItemLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FormName = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  color: #ea1d6f;
+  margin: 3rem 8rem 3rem 4rem;
+  font-size: 26px;
+  &:after {
+    content: '';
+    display: block;
+    position: absolute;
+    left: 0;
+    bottom: -10px;
+    width: 25px;
+    height: 4px;
+    background: orange;
+  }
+  @media (max-width: 700px) {
+    flex-direction: column;
+    margin: 1rem;
+  }
+`;
+
+export const FormContact = styled.div`
+  margin-top: auto;
+  font-size: 8px;
+  color: #888;
+`;
+
+export const FormInput = styled.div`
+  .group {
+    margin-bottom: 15px;
+  }
+  .input {
+    width: 100%;
+    padding: 10px 0;
+    background: none;
+    border: none;
+    border-bottom: 1px solid #666;
+    color: #ddd;
+    font-size: 14px;
+    text-transform: uppercase;
+    outline: none;
+    transition: border-color 0.2s;
+    &::placeholder {
+      color: #666;
+    }
+  }
+  .group-message {
+    margin-top: 40px;
+  }
+  .form-button {
+    margin-bottom: 0;
+    text-align: right;
+    color: red;
+    #send {
+      border: none;
+      color: red;
+      &:hover {
+        background: none;
+      }
+    }
+  }
+`;
